f5076a3b9378cce7d6c4fc9f5a6fdc51446a4ede
Anirudh Mahajan <anirudhmahajan18@gmail.com>
2022-10-02 13:54:48 +0530

    Updated Swagger Documentation

--- src/main/java/com/book/store/BookApplication.java
+++ src/main/java/com/book/store/BookApplication.java
@@ -2,7 +2,15 @@ package com.book.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -10,6 +18,30 @@ public class BookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
+    }
+
+    @Bean
+    public Docket swaggerConfiguration(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/*"))
+                .apis(RequestHandlerSelectors.basePackage("com.book"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails(){
+        return new ApiInfo(
+                "Book Store Api",
+                "Spring Boot Apis For E-commerce Store",
+                "1.0",
+                "https://anirudhmahajan.netlify.app",
+                new springfox.documentation.service.Contact("Anirudh Mahajan","https://anirudhmahajan.netlify.app","anirudhmahajan18@gmail.com"),
+                "API Code",
+                "https://github.com/anirudhmahajan77/book-ecommerce",
+                Collections.emptyList()
+        );
     }
 
 }
--- src/main/java/com/book/store/Controller/Product.java
+++ src/main/java/com/book/store/Controller/Product.java
@@ -1,8 +1,8 @@
 package com.book.store.Controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class Product {
-    @GetMapping("/all")
+    @RequestMapping(value = "/allproducts", method = RequestMethod.GET)
+    @ApiOperation(value = "Get All Products",
+            notes = "Get the List of all Products Available",
+            response = ResponseEntity.class)
     public ResponseEntity getAllProducts() {
 
         return new ResponseEntity<>("Hello", HttpStatus.OK);

5411b38abc7d868c24eeb23204de789e20f4cb62
Anirudh Mahajan <anirudhmahajan18@gmail.com>
2022-10-10 19:01:09 +0530

    Updated Swagger

--- src/main/java/com/book/store/BookApplication.java
+++ src/main/java/com/book/store/BookApplication.java
@@ -3,24 +3,24 @@ package com.book.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
+/*import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;*/
 
 import java.util.Collections;
 
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
 public class BookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
     }
 
-    @Bean
+    /*@Bean
     public Docket swaggerConfiguration(){
 
         return new Docket(DocumentationType.SWAGGER_2)
@@ -42,6 +42,6 @@ public class BookApplication {
                 "https://github.com/anirudhmahajan77/book-ecommerce",
                 Collections.emptyList()
         );
-    }
+    }*/
 
 }
--- src/main/java/com/book/store/Controller/AuthorController.java
+++ src/main/java/com/book/store/Controller/AuthorController.java
@@ -3,7 +3,8 @@ package com.book.store.Controller;
 import com.book.store.Model.Author;
 import com.book.store.Model.RequestModel.AddAuthor;
 import com.book.store.Service.AuthorService;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,27 +20,28 @@ public class AuthorController {
     AuthorService authorService;
 
     @PostMapping(value = "/addauthor")
-    @ApiOperation(value = "Add New Author",
+    /*@ApiOperation(value = "Add New Author",
             notes = "Add new author to store and make it available",
-            response = ResponseEntity.class)
+            response = ResponseEntity.class)*/
+    @Operation(summary = "foo", description = "bar")
     public ResponseEntity addAuthor(@RequestBody AddAuthor addAuthor) {
         Long result = authorService.addAuthor(addAuthor);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping(value = "/allauthors")
-    @ApiOperation(value = "Get All Author",
+    /*@ApiOperation(value = "Get All Author",
             notes = "Get All the listed Authors on the store",
-            response = ResponseEntity.class)
+            response = ResponseEntity.class)*/
     public ResponseEntity getAllAuthor() {
         List<Author> result = authorService.getAllAuthors();
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping(value = "/findauthor/{id}")
-    @ApiOperation(value = "Get Author By ID",
+    /*@ApiOperation(value = "Get Author By ID",
             notes = "Get the Authors on the basis of their ID",
-            response = ResponseEntity.class)
+            response = ResponseEntity.class)*/
     public ResponseEntity getAuthorById(@PathVariable("id") Long id) {
         Author result = authorService.getAuthorById(id);
         return new ResponseEntity<>(result, HttpStatus.OK);
